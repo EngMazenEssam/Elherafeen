be8e2b61f67a741dfa7b49f51f7097538d086f7d
MazenEssam <Mazenessampc@gmail.com>
2025-12-06 18:03:42 +0200

    For Diso

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,8 @@
 from flask import Flask
 from app.config import SECRET_KEY
-from app.routes.customer_routes import customer_bp
-
 
 def create_app() -> Flask:
-    app = Flask(__name__)
+    app = Flask(__name__, template_folder="templates", static_folder="static")
     app.secret_key = SECRET_KEY
 
     # تسجيل الـ blueprints
--- run.py
+++ run.py
@@ -1,12 +1,12 @@
 from flask import Flask
 from app.config import SECRET_KEY
 from app.routes.customer_routes import customer_bp
-from app import create_app
 
+app = Flask(__name__, template_folder="templates", static_folder="static")
+app.secret_key = SECRET_KEY
 
-from app import create_app
-
-app = create_app()
+# تسجيل الـ blueprints
+app.register_blueprint(customer_bp)
 
 if __name__ == "__main__":
     app.run(debug=True)
